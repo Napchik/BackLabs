81b324a8a3372f89d4ed1d7bc0b30c8c2bf8443d
Mikhail Shikalovskyi <mikhail232003@gmail.com>
2023-10-12 16:14:38 +0300

    adding healthcheck endpoint

--- src/views.py
+++ src/views.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
+from datetime import datetime
 
 app = Flask(__name__)
+
+health_status = True
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 
-@app.route("/test")
-def test():
-    return "<p>Works!</p>"
+@app.route("/healthcheck")
+def healthcheck():
+    if health_status:
+        resp = jsonify(date=datetime.now(), status="OK")
+        resp.status_code = 200
+    else:
+        resp = jsonify(date=datetime.now(), status="FAIL")
+        resp.status_code = 500
+    return resp
