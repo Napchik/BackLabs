921a39fa0057bbe0f85e2a51f2ddaa44ad6e7d05
Mikhail Shikalovskyi <mikhail232003@gmail.com>
2023-12-14 20:25:01 +0200

    Adding Dataclass Category + Category get/post/delete

--- src/data.py
+++ src/data.py
@@ -8,10 +8,23 @@ class User:
     id: str = field(default_factory=lambda: uuid.uuid4().hex)
 
 
+@dataclass
+class Category:
+    name: str
+    id: str = field(default_factory=lambda: uuid.uuid4().hex)
+
+
 def generate_users(names):
     for name in names:
         new_user = User(name)
         yield new_user.id, new_user
 
 
+def generate_categories(categories):
+    for category in categories:
+        new_category = Category(category)
+        yield new_category.id, new_category
+
+
 users = dict(generate_users(["Mikhail", "Andriy", "Den", "David"]))
+categories = dict(generate_categories(["Products", "Clothes", "Technologies"]))
--- src/views.py
+++ src/views.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from src.data import User, users
+from src.data import Category, categories
 import dataclasses
 from datetime import datetime
 
@@ -21,9 +22,22 @@ def create_user():
     return dataclasses.asdict(user)
 
 
+@app.post("/category")
+def create_category():
+    category_data = request.get_json()
+    category = Category(**category_data)
+    categories[category.id] = category
+    return dataclasses.asdict(category)
+
+
 @app.get("/user/<id>")
 def get_user(id):
     return dataclasses.asdict(users[id])
+
+
+@app.get("/category/<id>")
+def get_category(id):
+    return dataclasses.asdict(categories[id])
 
 
 @app.get("/users")
@@ -31,6 +45,16 @@ def get_users():
     return list(users.values())
 
 
+@app.get("/categories")
+def get_categories():
+    return list(categories.values())
+
+
 @app.delete("/user/<id>")
 def delete_user(id):
     return dataclasses.asdict(users.pop(id))
+
+
+@app.delete("/category/<id>")
+def delete_category(id):
+    return dataclasses.asdict(categories.pop(id))
