69a3cd583139024501e547dd68b9f10b9fc33cd1
Mikhail Shikalovskyi <mikhail232003@gmail.com>
2023-12-21 18:16:53 +0200

    Adding config.py + fully db connection + updating requirements

---
+++ src/config.py
@@ -0,0 +1,12 @@
+import os
+PROPAGATE_EXCEPTIONS = True
+FLASK_DEBUG = True
+
+
+db_user = os.environ['DB_USER']
+db_password = os.environ['DB_PASSWORD']
+db_host = os.environ['DB_HOST']
+db_name = os.environ['DB_NAME']
+SQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_password}@{db_host}/{db_name}"
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
