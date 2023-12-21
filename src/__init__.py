456365e4cf8d382e8d918df00e1910f19287ca93
Mikhail Shikalovskyi <mikhail232003@gmail.com>
2023-12-21 21:17:54 +0200

    Adding secret token + updating requirements

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_restful import Api
+from config import JWT_SECRET_KEY
 
 app = Flask(__name__)
 
 app.config.from_pyfile('config.py', silent=True)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+api = Api(app)
 
 import src.views
 import src.models
--- src/config.py
+++ src/config.py
@@ -1,12 +1,14 @@
 import os
+
 PROPAGATE_EXCEPTIONS = True
 FLASK_DEBUG = True
-
 
 db_user = os.environ['DB_USER']
 db_password = os.environ['DB_PASSWORD']
 db_host = os.environ['DB_HOST']
 db_name = os.environ['DB_NAME']
+JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
+JWT_ALGORITHM = ['HS256']
 SQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_password}@{db_host}/{db_name}"
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
